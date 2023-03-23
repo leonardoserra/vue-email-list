ac36316deab1f1327eb5d05eb2f4dc72f9d36e63
leonardoserra <serra.leonardo92@gmail.com>
2023-03-23 14:42:53 +0100

    importo vue e axios

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+/*
+    Descrizione:
+        Attraverso l’apposita API di Boolean
+        https://flynn.boolean.careers/exercises/api/random/mail
+        generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+    Bonus
+        Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        
       }
+    },
+    mounted(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function(response) {
+            const result = response.data;
+        });
     }
   }).mount('#app')
