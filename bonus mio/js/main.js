2b558e97636642542f8bcbc73d4784e20308adc4
leonardoserra <serra.leonardo92@gmail.com>
2023-03-23 15:53:48 +0100

    avviso con alert se metti numeri negativi

--- bonus mio/js/main.js
+++ bonus mio/js/main.js
@@ -19,9 +19,11 @@ const { createApp } = Vue
     methods:{
       generator(){
         this.results = [];
-        if(this.umberOfMails < 0){
+        if(this.numberOfMails < 0){
           this.numberOfMails = 0;
-          this.error = 'non inserire numeri sotto lo zero';
+          alert('ATTENZIONE: non inserire numeri sotto lo zero');
+          console.log(this.error)
+          console.log(this.numberOfMails)
         }else{
           for(i=0;i<this.numberOfMails;i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
@@ -36,7 +38,7 @@ const { createApp } = Vue
       }
     },
 
-    mounted(){
+    // mounted(){
     // for(i=0;i<10;i++){
     //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     //         .then((response)=> {
@@ -48,8 +50,8 @@ const { createApp } = Vue
                 
     //     });
     // }
-        console.log(this.results);
-    }
+    //     console.log(this.results);
+    // }
 
   }).mount('#app')
 
